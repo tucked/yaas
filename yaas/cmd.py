d55b8787cf1eb9420fab30d45fe28f2e3e5041ed
Karthik Palaniappan <karth295@uw.edu>
2015-09-24 23:07:41 -0700

    Removed the hello command, and layed the plumbing for the main subcommands

--- yaas/cmd.py
+++ yaas/cmd.py
@@ -6,24 +6,19 @@ import sys
 import os
 
 from . import __version__
+from . import blueprint
+from . import cluster
 from . import config
+from . import host
+from . import repo
+from . import service
+from . import task
 
 YAAS_VERSION = "yaas version {0}".format(__version__)
 
 def version(parser, args):
     """print the yaas version"""
     print YAAS_VERSION
-
-def hello(parser, args):
-    """show an elephant"""
-    print "                .-.._       "
-    print "          __  /`     '.     "
-    print "       .-'  `/   (   a \    "
-    print "      /      (    \,_   \   "
-    print "     /|       '---` |\ =|   "
-    print "    ` \    /__.-/  /  | |   "
-    print "       |  / / \ \  \   \_\  "
-    print "       |__|_|  |_|__\       "
 
 def main():
     config.server_url = os.environ.get('YAAS_SERVER_URL', config.server_url)
@@ -49,7 +44,12 @@ def main():
 
     commands = {
         'version' : version,
-        'hello' : hello,
+        'blueprint': blueprint.command,
+        'cluster': cluster.command,
+        'repo': repo.command,
+        'service': service.command,
+        'task': task.command,
+        'host': host.command,
         }
 
     for name, fn in commands.iteritems():
@@ -57,6 +57,10 @@ def main():
 
     args, extra = parser.parse_known_args(sys.argv[1:])
     config.args = args
+
+    if args.command != 'version' and config.server_url is None:
+        parser.error("Ambari server URL must be specified " \
+            "through environment variable YAAS_SERVER_URL")
 
     commands[args.command](subparsers.choices[args.command], extra)
 
