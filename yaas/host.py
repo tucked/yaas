7ec0038fde069d39eb7251f4cae2708923dc5be3
dtucker <david.michael.tucker@gmail.com>
2015-10-07 00:28:31 -0700

    Implement host list.

--- yaas/host.py
+++ yaas/host.py
@@ -1,4 +1,29 @@
 # coding: utf-8
 
+from __future__ import absolute_import
+from __future__ import print_function
+
+import inspect
+
+from . import config
+
+
 def command(parser, args):
-    pass
+    """ Interact with server agents. """
+    subparsers = parser.add_subparsers(dest='subcommand')
+    subcommands = {
+        'list': host_list
+        }
+    for name, fn in subcommands.items():
+        subparsers.add_parser(name, help=inspect.getdoc(fn))
+    subargs, extra = parser.parse_known_args(args)
+    subcommands[subargs.subcommand](
+        subparsers.choices[subargs.subcommand],
+        extra)
+
+
+@config.request('/api/v1/hosts')
+def host_list(parser, args, response):
+    """ List all server agents. """
+    for item in response.json()['items']:
+        print(item['Hosts']['host_name'])
