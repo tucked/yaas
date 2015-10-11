4231052c5120129d6ead42350d29e63ebca8bd85
dtucker <david.michael.tucker@gmail.com>
2015-10-10 21:30:47 -0700

    Implement host list --fields.

--- yaas/host.py
+++ yaas/host.py
@@ -27,8 +27,13 @@ def command(parser, args):
 
 def _list(parser, args):
     """ List all registered hosts. """
+    parser.add_argument(
+        '--fields',
+        help="Print host details.")
+    subargs, extra = parser.parse_known_args(args)
     response = requests.get(
         config.href('/api/v1/hosts'),
+        params={'fields': subargs.fields},
         **config.requests_opts())
     if config.args.raw:
         pprint.pprint(response.json())
@@ -39,4 +44,13 @@ def _list(parser, args):
         print(response.json()['message'], file=sys.stderr)
         sys.exit(1)
     for item in response.json()['items']:
-        print(item['Hosts']['host_name'])
+        line = []
+        line_keys = ['host_name', 'cluster_name']
+        for key in line_keys:
+            if key in item['Hosts']:
+                line.append(item['Hosts'][key])
+        print(' - '.join(line))
+        for key, value in item['Hosts'].items():
+            if key not in line_keys:
+                print('{indent}{key}: {value}'.format(
+                    indent=' '*4, key=key, value=value))
