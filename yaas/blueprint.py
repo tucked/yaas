e3ce812ffb7ebbbe958475c2755139d23396a4a6
dtucker <david.michael.tucker@gmail.com>
2015-10-17 21:28:00 -0700

    Implement blueprint add.

--- yaas/blueprint.py
+++ yaas/blueprint.py
@@ -3,7 +3,9 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import argparse
 import inspect
+import json
 import pprint
 import requests
 import sys
@@ -15,6 +17,7 @@ def command(parser, args):
     """ Interact with Ambari blueprints. """
     subparsers = parser.add_subparsers(dest='subcommand')
     subcommands = {
+        'add': _add,
         'list': _list,
         }
     for name, fn in subcommands.items():
@@ -23,6 +26,32 @@ def command(parser, args):
     subcommands[subargs.subcommand](
         subparsers.choices[subargs.subcommand],
         extra)
+
+
+def _add(parser, args):
+    """ Add a blueprint to the Ambari server. """
+    parser.add_argument(
+        '--validate-topology',
+        help="Specify whether to validate a blueprint on upload.")
+    parser.add_argument(
+        'blueprint',
+        nargs='*',
+        help="Specify a blueprint to add to the Ambari server.")
+    subargs = parser.parse_args(args)
+    blueprints = [] if len(subargs.blueprint) > 0 else [sys.stdin.read()]
+    for blueprint_file in subargs.blueprint:
+        with open(blueprint_file, 'r') as f:
+            blueprints.append(f.read())
+    for blueprint in blueprints:
+        blueprint = json.loads(blueprint)
+        response = requests.post(
+            config.href(
+                '/api/v1/blueprints/{name}'.format(
+                    name=blueprint['Blueprints']['blueprint_name'])),
+            data=json.dumps(blueprint),  # `json=blueprint` would be better, but it doesn't work!
+            params={'validate_topology': subargs.validate_topology},
+            **config.requests_opts())
+        response.raise_for_status()
 
 
 def _list(parser, args):
